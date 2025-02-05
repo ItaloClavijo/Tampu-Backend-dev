5e10baff1500fbdb3aa50363f6793b7343946e2e
ItaloClavijo <u201711622@upc.edu.pe>
2025-02-04 20:34:35 -0500

    First Commit

---
+++ src/main/java/pe/edu/upc/tampubackend/TampuBackendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tampubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TampuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TampuBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/upc/tampubackend/TampuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.tampubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TampuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
