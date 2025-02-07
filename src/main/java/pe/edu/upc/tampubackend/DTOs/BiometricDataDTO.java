fe2fde9dcc1ad295ec2058d1255d52f811ffe5a2
EdeerJ <u20201b780@upc.edu.pe>
2025-02-07 18:52:53 -0500

    Set y get en DTO

--- src/main/java/pe/edu/upc/tampubackend/DTOs/BiometricDataDTO.java
+++ src/main/java/pe/edu/upc/tampubackend/DTOs/BiometricDataDTO.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.tampubackend.DTOs;
 
 import lombok.Data;
+import pe.edu.upc.tampubackend.Entities.BiometricData;
 import pe.edu.upc.tampubackend.Entities.Users;
 
 import java.time.LocalDateTime;
@@ -12,4 +13,36 @@ public class BiometricDataDTO {
     private double bloodPressure;
     private double spo2;
     private LocalDateTime date;
+    private double stresslevel;
+
+    public BiometricDataDTO() {}
+
+    public BiometricDataDTO( double heartRate, double bloodPressure, double spo2, LocalDateTime date, double stresslevel ) {
+
+        this.heartRate = heartRate;
+        this.bloodPressure = bloodPressure;
+        this.spo2 = spo2;
+        this.date = date;
+        this.stresslevel = stresslevel;
+
+    }
+
+    public double getHeartRate() { return heartRate; }
+    public void setHeartRate( double heartRate ) { this.heartRate = heartRate; }
+
+    public double getBloodPressure() { return bloodPressure; }
+    public void setBloodPressure ( double bloodPressure ) { this.bloodPressure = bloodPressure; }
+
+    public double getSpo2() { return spo2; }
+    public void setSpo2 ( double spo2 ) { this.spo2 = spo2; }
+
+    public LocalDateTime getDate() { return date; }
+    public void setDate ( LocalDateTime date ) { this.date = date; }
+
+    public double getStresslevel() { return stresslevel; }
+    public void setStresslevel ( double stresslevel ) { this.stresslevel = stresslevel; }
+
+
 }
+
+
